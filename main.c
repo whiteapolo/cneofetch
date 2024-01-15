3b1597700da2faed8cfcac012cf466dc7e4c5899
whiteapolo <140943505+whiteapolo@users.noreply.github.com>
2024-01-15 17:46:48 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,31 +1,58 @@
 #include <stdio.h>
+#include <string.h>
 #include "modules.h"
 
 #define MODULES_COUNT 8
 
+void remove_empty_modules(char offsets[MODULES_COUNT], char modules[MODULES_COUNT][BUFFER_LEN])
+{
+	char tmp;
+	int j = 0;
+	for (int i = 0; i < MODULES_COUNT; i++) {
+		if (modules[i][0])
+			offsets[j++] = i;
+		else 
+			tmp = i;
+	}
+	while (j < MODULES_COUNT)
+		offsets[j++] = tmp;
+}
+
 void main(void)
 {
-	  char modules[MODULES_COUNT][BUFFER_LEN];
-	  get_os(modules[0]);
-	  get_cpu(modules[1]);
-	  get_battery(modules[2]);
-	  get_kernel(modules[3]);
-	  get_desktop(modules[4]);
-	  get_shell(modules[5]);
-	  get_packages(modules[6]);
-	  get_uptime(modules[7]);
+	char modules[MODULES_COUNT][BUFFER_LEN] = {0};
+	char offsets[MODULES_COUNT];
 
-	  printf(
-	  "         \x1b[90m#####\n"
-	  "        #######\n"
-	  "        ##\e[0m0\x1b[90m#\e[0m0\x1b[90m##             \e[1;91m│\e[0m %s\n"
-	  "        \x1b[90m#\x1b[33m#####\x1b[90m#             \e[1;92m│\e[0m %s\n"
-	  "      \x1b[90m##\e[0m##\x1b[33m###\e[0m##\x1b[90m##           \e[1;93m│\e[0m %s\n"
-	  "     \x1b[90m#\e[0m##########\x1b[90m##          \e[1;94m│\e[0m %s\n"
-	  "    \x1b[90m#\e[0m############\x1b[90m##         \e[1;95m│\e[0m %s\n"
-	  "    \x1b[90m#\e[0m###########\x1b[90m####        \e[1;96m│\e[0m %s\n"
-	  "   \x1b[33m##\x1b[90m#\e[0m###########\x1b[90m##\x1b[33m#        \e[1;97m│\e[0m %s\n"
-	  " \x1b[33m######\x1b[90m#\e[0m#######\x1b[90m#\x1b[33m######      \e[1;90m│\e[0m %s\n"
-	  " \x1b[33m#######\x1b[90m#\e[0m#####\x1b[90m#\x1b[33m#######\n"
-	  "   #####\x1b[90m#######\x1b[33m#####\n\e[0m", modules[0], modules[1], modules[2], modules[3], modules[4], modules[5], modules[6], modules[7]);
+	get_os(modules[0]);
+	get_cpu(modules[1]);
+	get_battery(modules[2]);
+	get_kernel(modules[3]);
+	get_desktop(modules[4]);
+	get_shell(modules[5]);
+	get_packages(modules[6]);
+	get_uptime(modules[7]);
+
+	remove_empty_modules(offsets, modules);
+
+	printf(
+	"         \x1b[90m#####\n"
+	"        #######\n"
+	"        ##\e[0m0\x1b[90m#\e[0m0\x1b[90m##             %s\n"
+	"        \x1b[90m#\x1b[33m#####\x1b[90m#             %s\n"
+	"      \x1b[90m##\e[0m##\x1b[33m###\e[0m##\x1b[90m##           %s\n"
+	"     \x1b[90m#\e[0m##########\x1b[90m##          %s\n"
+	"    \x1b[90m#\e[0m############\x1b[90m##         %s\n"
+	"    \x1b[90m#\e[0m###########\x1b[90m####        %s\n"
+	"   \x1b[33m##\x1b[90m#\e[0m###########\x1b[90m##\x1b[33m#        %s\n"
+	" \x1b[33m######\x1b[90m#\e[0m#######\x1b[90m#\x1b[33m######      %s\n"
+	" \x1b[33m#######\x1b[90m#\e[0m#####\x1b[90m#\x1b[33m#######\n"
+	"   #####\x1b[90m#######\x1b[33m#####\n\e[0m",
+	modules[offsets[0]], 
+	modules[offsets[1]], 
+	modules[offsets[2]], 
+	modules[offsets[3]], 
+	modules[offsets[4]], 
+	modules[offsets[5]], 
+	modules[offsets[6]], 
+	modules[offsets[7]]);
 }
--- modules.h
+++ modules.h
@@ -7,9 +7,20 @@
 #include <sys/utsname.h>
 #include <stdio.h>
 
-#define PACKAGE_QUERY "dpkg-query -f '${binary:Package}\n' -W | wc -l"
-#define BUFFER_LEN 80
-#define CPU_MODEL_SPACES 3
+#define PACKAGES_QUERY "dpkg-query -f '${binary:Package}\n' -W | wc -l"
+#define BUFFER_LEN 70
+#define CPU_MODELWORDS 3
+
+#define OS_LABEL	"\e[1;91m│\e[0m \x1b[1;31mOS:\x1b[0m %s"
+#define CPU_LABEL	"\e[1;92m│\e[0m \x1b[1;32mCPU:\x1b[0m %s"
+#define BATTERY_LABEL	"\e[1;93m│\e[0m \x1b[1;33mBattery:\x1b[0m %s"
+#define KERNEL_LABEL	"\e[1;94m│\e[0m \x1b[1;34mKernel:\x1b[0m %s"
+#define DESKTOP_LABEL	"\e[1;95m│\e[0m \x1b[1;35mdesktop:\x1b[0m %s"
+#define SHELL_LABEL	"\e[1;96m│\e[0m \x1b[1;36mshell:\x1b[0m %s"
+#define PACKAGES_LABEL  "\e[1;97m│\e[0m \x1b[1;37mPackages:\x1b[0m %s"
+#define UPTIME_LABEL    "\e[1;90m│\e[0m \x1b[1;30mUptime:\x1b[0m"
+
+
 #define ASSERT(a) if(a) return;
 
 static char buffer[BUFFER_LEN];
@@ -27,7 +38,7 @@ void get_shell(char *dest);
 void get_uptime(char *dest);
 void get_packages(char *dest);
 
-char *strnchr(char *str, char c, int n);
+char *_strnchr(char *str, char c, int n);
 void _str_lower(char *str);
 void _replace_char_in_string(char *str, char from, char to);
 int _read_line_from_file(const char fileName[], unsigned short line, char *buffer, unsigned char bufferLength);
@@ -41,23 +52,22 @@ int _read_substring_line(const char fileName[], const char *sub, char *buffer, u
 void get_os(char *dest)
 {
 	  ASSERT(_read_substring_line("/etc/os-release", "PRETTY_NAME", buffer, BUFFER_LEN));
-
 	  char *start = strchr(buffer, '"');
 	  char *end = strrchr(buffer, '"');
 	  ASSERT(!end || !start);
 	  *end = '\0';
 
-	  sprintf(dest, "\x1b[1;31mOS:\x1b[0m %s", ++start);
+	  sprintf(dest, OS_LABEL, ++start);
 }
 
 void get_cpu(char *dest)
 {
 	ASSERT(_read_line_from_file("/proc/cpuinfo", 5, buffer, BUFFER_LEN));
 	char *start = strchr(buffer, ':');
-	char *end = strnchr(start + 2, ' ', CPU_MODEL_SPACES);
+	char *end = _strnchr(start + 2, ' ', CPU_MODELWORDS);
 	ASSERT(!start || !end);
 	*end = '\0';
-	sprintf(dest, "\x1b[1;32mCPU:\x1b[0m %s", start + 2);
+	sprintf(dest,CPU_LABEL, start + 2);
 }
 
 
@@ -65,21 +75,21 @@ void get_battery(char *dest)
 {
 	ASSERT(_read_line_from_file("/sys/class/power_supply/BAT0/capacity", 1, buffer, BUFFER_LEN));
 	_replace_char_in_string(buffer, '\n', '%');
-	sprintf(dest, "\x1b[1;33mBattery:\x1b[0m %s", buffer);
+	sprintf(dest, BATTERY_LABEL, buffer);
 }
 
 void get_kernel(char *dest)
 {
 	  struct utsname info;
 	  ASSERT(uname(&info));
-	  sprintf(dest, "\x1b[1;34mKernel:\x1b[0m %s", info.release);
+	  sprintf(dest, KERNEL_LABEL, info.release);
 }
 
 void get_desktop(char *dest)
 {
 	  const char *desktop = getenv("XDG_CURRENT_DESKTOP");
 	  ASSERT(!desktop);
-	  sprintf(dest, "\x1b[1;35mDesktop:\x1b[0m %s", desktop);
+	  sprintf(dest, DESKTOP_LABEL, desktop);
 	  _str_lower(dest);
 }
 
@@ -87,13 +97,13 @@ void get_shell(char *dest)
 {
 	  const char *shell = getenv("SHELL");
 	  ASSERT(!shell);
-	  sprintf(dest,  "\x1b[1;36mShell:\x1b[0m %s", shell);
+	  sprintf(dest, SHELL_LABEL, shell);
 }
 
 void get_packages(char *dest)
 {
 	FILE *fp;
-	fp = popen(PACKAGE_QUERY, "r");
+	fp = popen(PACKAGES_QUERY, "r");
 	ASSERT(!fp);
 
 	fgets(buffer, 8, fp);
@@ -101,7 +111,7 @@ void get_packages(char *dest)
 	char *tmp = strchr(buffer, '\n');
 	if(tmp)
 		*tmp = '\0';
-	sprintf(dest,  "\x1b[1;37mPackages:\x1b[0m %s", buffer);
+	sprintf(dest, PACKAGES_LABEL, buffer);
 }
 
 void get_uptime(char *dest)
@@ -118,8 +128,8 @@ void get_uptime(char *dest)
 	int hours = (seconds / 3600) % 24;
 	int mins = (seconds / 60) % 60;
 
-	strcpy(dest, "\x1b[1;30mUptime:\x1b[0m");
-	char *tmp = &dest[strlen(dest) - 1];
+	strcpy(dest, UPTIME_LABEL);
+	char *tmp = &dest[strlen(dest)];
 
 	if (days)
 		sprintf(tmp, " %d days,", days);
@@ -137,7 +147,7 @@ void get_uptime(char *dest)
  *                       LOCAL FUNCTIONS                              *
  *********************************************************************/
 
-char *strnchr(char *str, char c, int n)
+char *_strnchr(char *str, char c, int n)
 {
 	char *result = str;
 	for (int i = 0; i < n; i++) {
